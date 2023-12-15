06a86855c839983040e35593c4221ec4f61a25a4
wangshunnn <wangshunnn@gmail.com>
2023-12-16 00:32:31 +0800

    update

--- 板子/树状dp.ts
+++ 板子/树状dp.ts
@@ -3,12 +3,12 @@
  * * LC 307
  */
 class NumArray {
-    private nums: number[];
-    private tree: number[];
+    private nums: number[]
+    private tree: number[]
     constructor(nums: number[]) {
-        this.nums = new Array(nums.length).fill(0);
-        this.tree = new Array(nums.length + 1).fill(0);
-        this.initTree(nums);
+        this.nums = new Array(nums.length).fill(0)
+        this.tree = new Array(nums.length + 1).fill(0)
+        this.initTree(nums)
     }
 
     /**
@@ -17,7 +17,7 @@ class NumArray {
      */
     private initTree(nums: number[]): void {
         for (let i = 0; i < nums.length; i++) {
-            this.update(i, nums[i]);
+            this.update(i, nums[i])
         }
     }
 
@@ -25,28 +25,28 @@ class NumArray {
      * lowbit 算法，方便计算父节点
      */
     private lowbit(num: number): number {
-        return num & -num;
+        return num & -num
     }
 
     /**
      * 动态维护前缀和 O(logn)
      */
     private prefixSum(index: number): number {
-        let sum = 0;
+        let sum = 0
         for (let i = index; i > 0; i -= this.lowbit(i)) {
-            sum += this.tree[i];
+            sum += this.tree[i]
         }
-        return sum;
+        return sum
     }
 
     /**
      * 单点修改 O(logn)
      */
     update(index: number, val: number): void {
-        const delta = val - this.nums[index];
-        this.nums[index] = val;
+        const delta = val - this.nums[index]
+        this.nums[index] = val
         for (let i = index + 1; i < this.tree.length; i += this.lowbit(i)) {
-            this.tree[i] += delta;
+            this.tree[i] += delta
         }
     }
 
@@ -54,7 +54,7 @@ class NumArray {
      * 区间查询 O(logn)
      */
     sumRange(left: number, right: number): number {
-        return this.prefixSum(right + 1) - this.prefixSum(left);
+        return this.prefixSum(right + 1) - this.prefixSum(left)
     }
 }
 
