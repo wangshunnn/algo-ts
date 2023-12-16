7ba4ddc0687264e66a093c8ed1478c8de3ccf36c
wangshunnn <wangshunnn@gmail.com>
2023-12-16 18:28:50 +0800

    update

--- 板子/并查集.ts
+++ 板子/并查集.ts
@@ -3,15 +3,17 @@
  */
 class UnionFind {
     private fa: number[] // 每个节点的祖先，初始化为自己
-    private size: number[] // 每个连通分量的数量
-    private setCount: number // 当前连通分量的节点数（可选）
+    private size: number[] // 每个连通分量的节点数量
+    private setCount: number // 所有连通分量的个数（可选）
 
+    /** 初始化 */
     constructor(n: number) {
         this.fa = new Array(n).fill(-1).map((_, i) => i)
         this.size = new Array(n).fill(1)
         this.setCount = n
     }
 
+    /** 查询 */
     find(x: number): number {
         if (this.fa[x] !== x) {
             // 路径压缩
@@ -20,6 +22,7 @@ class UnionFind {
         return this.fa[x]
     }
 
+    /** 合并 */
     union(a: number, b: number): boolean {
         const pa = this.find(a)
         const pb = this.find(b)
@@ -38,7 +41,13 @@ class UnionFind {
         return true
     }
 
+    /** 判断是否连通 */
     connected(a: number, b: number): boolean {
         return this.find(a) === this.find(b)
+    }
+
+    /** 查询当前所在联通分量的节点数 */
+    getSize(x: number): number {
+        return this.size[this.find(x)]
     }
 }
