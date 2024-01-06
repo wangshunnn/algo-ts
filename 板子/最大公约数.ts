0b6baa65d83a8623a3a6fe61b1c9ee2a0e97f860
wangshunnn <wangshunnn@gmail.com>
2024-01-06 17:17:58 +0800

    update

---
+++ 板子/最大公约数.ts
@@ -0,0 +1,21 @@
+// @ts-nocheck
+
+// 最大公约数（Greatest Common Divisor, GCD）
+// 辗转相除法（欧几里得算法）
+
+// 递归
+function gcd(a: number, b: number): number {
+    if (b === 0) {
+        return a
+    } else {
+        return gcd(b, a % b)
+    }
+}
+
+// 迭代
+function gcd(a: number, b: number): number {
+    while (b !== 0) {
+        ;[a, b] = [b, a % b]
+    }
+    return a
+}
