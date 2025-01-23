78ef1887c8a534d66aa0a5178fbb74012137ff65
wangshunnn <wangshunnn@gmail.com>
2025-01-23 23:18:29 +0800

    feat: update

--- src/字符串/manacher.ts
+++ src/字符串/manacher.ts
@@ -52,10 +52,30 @@ function longestPalindrome(s: string) {
 
   // 返回每个中心点的回文半径（不包含中心，注意包含 '#'）
   // f[i] >> 1 表示原字符串中的半径，注意奇偶数的中心点差异
+
   return f
   // 返回最长回文字串长度，如果不需要可以去掉
   // return [...s]
   //   .slice(start, end)
   //   .filter(v => v !== '#')
   //   .join('')
+}
+
+/**
+ * 常用场景：判断原字符串 s 中下标 [l, r] 的子字符串是否是回文串
+ */
+
+// 提前计算好 f 数组
+const s = '..'
+const f = longestPalindrome(s)
+
+function isPalindrome(l: number, r: number) {
+  // [l, r] 对应插入 # 之后子符串的中心点 l+r+1
+  const center = l + r + 1 // r-l
+  // [l, r] 对应插入 # 之后子字符串的半径长度
+  const arm = r - l
+  if (f[center] >= arm) {
+    return true
+  }
+  return false
 }
